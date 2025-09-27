4d156b890061a0cdbc66fcf3ab61ac7b2171d101
Goofball7705 <157848971+Goofball7705@users.noreply.github.com>
2025-09-26 22:08:24 -0400

    Up and down movement / fireable laser / textured player model

--- GDW Project One/Assets/Scripts/PlayerController.cs
+++ GDW Project One/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float verticalInput;
     public float horizontalInput;
     public float speed = 10.0f;
+    public GameObject Laser;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,10 +18,21 @@ public class PlayerController : MonoBehaviour
     void Update()
 
     {
+        //checks for up down left and right input
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up *  verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Laser, transform.position, Laser.transform.rotation);
+        }
+
+
+        //keeps the player in bounds
         if (transform.position.z < -14)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -14);
@@ -30,5 +43,14 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, 39);
         }
 
+        if (transform.position.y > 27)
+        {
+            transform.position = new Vector3(transform.position.x, 27, transform.position.z);
+        }
+
+        if (transform.position.y < 3)
+        {
+            transform.position = new Vector3(transform.position.x, 3, transform.position.z);
+        }
     }
 }
